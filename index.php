57eb94cb1c197ddbe5a6818ac48d29eef5a97419
CloudSalander <ruben.as532@gmail.com>
2025-11-20 10:47:10 +0100

    Initial plan

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+//Note: Assuming english language
+//Todo: generalize to other langueages
+
+define('VOWELS',['a','e','i','o','u']);
+
+function getVowelsCount(string $word): array {
+    return [];
+}
+
+function printVowelsCount(array $vowelsCount): void {
+
+}
+
+var_dump(VOWELS);
+var_dump(getVowelsCount(""));
+var_dump(printVowelsCount([]));
+
