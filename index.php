a27f5b424abc710115a855818ef03ec905b5b462
CloudSalander <ruben.as532@gmail.com>
2025-11-20 10:52:16 +0100

    Word/text loop

--- index.php
+++ index.php
@@ -5,14 +5,30 @@
 define('VOWELS',['a','e','i','o','u']);
 
 function getVowelsCount(string $word): array {
-    return [];
+    $vowelsCount = [
+        "a" => 0,
+        "e" => 0,
+        "i" => 0,
+        "o" => 0,
+        "u" => 0
+    ];
+
+    $word = strtolower($word);
+    $word = str_split($word);
+
+
+    foreach($word as $char) {
+        echo $char;
+    }
+
+    return $vowelsCount;
 }
 
 function printVowelsCount(array $vowelsCount): void {
 
 }
 
-var_dump(VOWELS);
-var_dump(getVowelsCount(""));
-var_dump(printVowelsCount([]));
+//var_dump(VOWELS);
+var_dump(getVowelsCount("hola"));
+//var_dump(printVowelsCount([]));
 
